e51bc870642f1ef693d09553e9e188737d96d783
Pokusaeff Daniil <pokusaeff.daniil@gmail.com>
2021-10-26 08:46:39 +0300

    feat: add route to 404

--- config/routes.php
+++ config/routes.php
@@ -2,5 +2,7 @@
 return [
 	'news/([0-9]+)' => 'news/view/$1', // actionView in NewsController | Просмотр одной новости
 	'news/page-([0-9]+)' => 'news/index/$1', // actionIndex in NewsController | Просмотр новостей страницы
-	'news' => 'news/index/1', // actionIndex in NewsController | Просмотр новостей 1й страницы
+	'\b(news)\b' => 'news/index/1', // actionIndex in NewsController | Просмотр новостей 1й страницы
+
+	'not-found' => "news/error404" // actionError404 in NewsController | Неправильная страница
 ];
