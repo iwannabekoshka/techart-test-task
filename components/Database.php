a40daa06e8ef269bc22becf28b572dd3c9043cdb
Pokusaeff Daniil <pokusaeff.daniil@gmail.com>
2021-10-26 08:41:50 +0300

    fix: fix charset

--- components/Database.php
+++ components/Database.php
@@ -8,7 +8,7 @@ class Database
 		$paramsPath = ROOT.'/config/db_params.php';
 		$params = include($paramsPath);
 
-		$dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
+		$dsn = "mysql:host={$params['host']};dbname={$params['dbname']};charset=utf8";
 		$db = new PDO($dsn, $params['user'], $params['password']);
 
 		return $db;
